c10a64bc2ddc3b09ff7277186078556b4372e06d
Emmy Andersson <emmy.andersson.win14@HermodsAB.onmicrosoft.com>
2016-01-19 22:28:24 +0100

    Fixat BMI logik i tjänsterna

---
+++ LABB2 - Tjänster och Service/WCF/BmiCalculator.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="BmiCalculator.asmx.cs" Class="WCF.BmiCalculator" %>
---
+++ LABB2 - Tjänster och Service/WCF/BmiCalculator.asmx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace WCF
+{
+    /// <summary>
+    /// Summary description for BmiCalculator
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class BmiCalculator : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string Calculator(int height, int weight)
+        {
+            return (weight/(height*height)).ToString();
+        }
+    }
+}
