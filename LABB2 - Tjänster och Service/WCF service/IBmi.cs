5e7910060cdce094a9eb6380c308de9f38f55e81
Emmy Andersson <emmy.andersson.win14@HermodsAB.onmicrosoft.com>
2016-01-19 21:24:21 +0100

    Starting with LABB 2 uppg 1

---
+++ LABB2 - Tjänster och Service/WCF service/IBmi.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCF_service
+{
+    [ServiceContract(Namespace = "http://WCF_Service")]
+    interface IBmi
+    {
+        [OperationContract]
+        string Calculate(int height, int weight);
+    }
+}
---
+++ LABB2 - Tjänster och Service/WCF service/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCF_service
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Base Address
+            Uri baseAddress = new Uri("http://localhost:8080/WCF_service");
+            //ServiceHost
+            ServiceHost selfServiceHost = new ServiceHost(typeof(BmiCheckerService), baseAddress);
+
+            try
+            {
+                //ABC  Endpoint
+                selfServiceHost.AddServiceEndpoint(typeof(IBmi), new WSHttpBinding(), "BmiCheckerService");
+
+                //Metadata för att hitta servicen och visa WSDL dokumentet.
+                ServiceMetadataBehavior smBehavior = new ServiceMetadataBehavior { HttpGetEnabled = true };
+                selfServiceHost.Description.Behaviors.Add(smBehavior);
+
+                //Startar tjänsten
+                selfServiceHost.Open();
+                Console.WriteLine("Tjänsten är öppen.");
+                Console.ReadKey();
+            }
+            catch (Exception)
+            {
+                
+                throw;
+            }
+        }
+    }
+}
