9e685f508637947d2cbb66f2f6e9fa84ee8001b6
Emmy Andersson <emmy.andersson.win14@HermodsAB.onmicrosoft.com>
2016-01-20 21:13:06 +0100

    Klar med uppift 4 med self service + klient

---
+++ LABB2 - Tjänster och Service/SelfService.HarryPotter/HarryPotterBooksCheckerService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelfService.HarryPotter
+{
+    class HarryPotterBooksCheckerService : IHarryPotterBooks
+    {
+        public string BookName(string name)
+        {
+            var books = new List<string>() {"Harry Potter and the Philosophers Stone", "Harry Potter and the Chamber of Secrets", "Harry Potter and the Prisoner of Azkaban", "Harry Potter and the Goblet of Fire", "Harry Potter and the Order of the Phoenix", "Harry Potter and the Half Blood Prince", "Harry Potter and the Deathly Hallows"};
+            foreach (var book in books)
+            {
+                if (book.ToLower().Contains(name.ToLower()))
+                {
+                    return book;
+                }
+               
+            }
+            return "No book contains that name.";
+        }
+    }
+}
---
+++ LABB2 - Tjänster och Service/SelfService.HarryPotter/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelfService.HarryPotter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //BaseAddress
+            Uri baseAddress = new Uri("http://localhost:8080/SelfService.HarryPotter");
+
+            //ServiceHost
+            ServiceHost selfServiceHost = new ServiceHost(typeof(HarryPotterBooksCheckerService), baseAddress);
+            try
+            {
+                //ABC-Endpoint
+                selfServiceHost.AddServiceEndpoint(typeof(IHarryPotterBooks), new WSHttpBinding(), "HarryPotterCheckerService");
+
+                ServiceMetadataBehavior smBehavior = new ServiceMetadataBehavior {HttpGetEnabled = true};
+                selfServiceHost.Description.Behaviors.Add(smBehavior);
+
+                selfServiceHost.Open();
+                Console.WriteLine("Tjänsten är öppen");
+                Console.ReadKey();
+
+            }
+            catch (CommunicationException ex)
+            {
+                selfServiceHost.Close();
+                Console.WriteLine($"Ett kommunikationsfel har uppstått.: {ex.Message}");
+                Console.ReadKey();
+                throw;
+            }
+        }
+    }
+}
