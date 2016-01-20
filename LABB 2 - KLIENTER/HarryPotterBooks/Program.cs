e47f6a079c59e8944307813595a5ab5e0e70d620
Emmy Andersson <emmy.andersson.win14@HermodsAB.onmicrosoft.com>
2016-01-20 21:22:14 +0100

    Klar med labb2

---
+++ LABB 2 - KLIENTER/HarryPotterBooks/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HarryPotterBooks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HarryPotterBooksClient client = new HarryPotterBooksClient();
+            Console.WriteLine("Skriv ett ord som finns i Harry Potter böckernas titel");
+            Console.WriteLine(client.BookName(Console.ReadLine()));
+            Console.ReadKey();
+        }
+    }
+}
